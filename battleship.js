72a3d6bc005beff6a22362e324d0a3340c336f2a
Luwi-xyz <atumanon@filamer.edu.ph>
2025-03-09 20:55:47 -0700

    battleshipv1

---
+++ battleship.js
@@ -0,0 +1,46 @@
+// Declared Var
+
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+ 
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+// Game Logic
+
+while (isSunk == false) {
+    guess = prompt("Ready!, Aim!, Fire! (Enter number 0-6)");
+
+    if (guess < 0 || guess > 6 ){
+        alert("Error! Please Enter a Valid Number.")
+    
+    } else {
+        guesses += 1;
+
+        if (guess == location1 || guess == location2  || guess == location3){
+            hits +=1;
+            alert("Hit!");
+        
+
+
+            if (hits == 3){
+                isSunk = true;
+                alert (" You Sunk the Ship!")
+            }else{
+                alert("I Miss You!")
+            }
+            }
+                
+
+        }
+    }
+
+
+var stats = "You sank my battership at " + guesses + " guesses" +
+"and your accuracy is" + (3/guesses);
+
+alert(stats);
