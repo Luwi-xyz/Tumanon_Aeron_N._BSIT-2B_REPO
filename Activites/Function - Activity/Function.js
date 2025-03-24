328347bc7d937fabeec04074a132461c1fb1fce0
Luwi-xzy <atumanon@filamer.edu.ph>
2025-03-23 20:16:15 -0700

    Activities

---
+++ Activites/Function - Activity/Function.js
@@ -0,0 +1,13 @@
+function whatShalliwear(temp){
+    if(temp <60){
+        console.log("Wear a jacket");
+    }else if (temp < 70){
+        console.log("Wear a sweater");
+    }else{
+        console.log("Wear a t-shirt");
+    }
+    
+}
+whatShalliwear(50);
+whatShalliwear(70);
+whatShalliwear(60);
