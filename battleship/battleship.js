f9ce6ddbae034ecc167d4c7f76f69f423ead09f0
Luwi-xyz <atumanon@filamer.edu.ph>
2025-04-20 23:03:35 +0800

    battleship_v3

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,46 +1,35 @@
-// Declared Var
-
+//Declared Variables
 var location1 = 3;
 var location2 = 4;
 var location3 = 5;
- 
+
 var guess;
 var hits = 0;
 var guesses = 0;
 
 var isSunk = false;
 
-// Game Logic
+//game logic
 
-while (isSunk == false) {
-    guess = prompt("Ready!, Aim!, Fire! (Enter number 0-6)");
+while (isSunk == false){
+    guess = prompt("Ready!, Aim!, Fire!(Enter number 0-6)")
 
-    if (guess < 0 || guess > 6 ){
-        alert("Error! Please Enter a Valid Number.")
-    
+    if (guess <0 || guess >6) {
+        alert ("Please enter a valid number!")
     } else {
-        guesses += 1;
+        guesses = guesses + 1;
 
-        if (guess == location1 || guess == location2  || guess == location3){
-            hits +=1;
-            alert("Hit!");
-        
-
+        if (guess == location1 || guess == location2 || guess == location3){
+            hits = hits + 1;
+            alert("Hit!")
 
             if (hits == 3){
                 isSunk = true;
-                alert (" You Sunk the Ship!")
-            }else{
-                alert("Miss!")
+                alert("You sank my battleship!")
             }
-            }
-                
-
+        }else{
+            alert("Miss.")
         }
     }
-
-
-var stats = "You sank my battership at " + guesses + " guesses" +
-"and your accuracy is" + (3/guesses);
-
-alert(stats);
+}
+var stats = "you sank my battleship at" + guesses + "guesses" + "and your acccuracy is" + (3/guesses);
--- battleship/battleship_v2.js
+++ battleship/battleship_v2.js
@@ -1,11 +1,11 @@
 // Declared Var
 
-var randomLoc = Math.floor(Math.random() *5);
+var randomLoc = Math.floor(Math.random() * 5)
 
 var location1 = randomLoc;
 var location2 = location1 + 1;
 var location3 = location2 + 1;
- 
+
 var guess;
 var hits = 0;
 var guesses = 0;
@@ -14,42 +14,37 @@ var isSunk = false;
 
 // Game Logic
 
-while (isSunk == false) {
+while (!isSunk) {
     guess = prompt("Ready!, Aim!, Fire! (Enter number 0-6)");
-    if (guess == null){
-        alert("k salamat!");
+
+    if (guess == null) {
+        alert("Nd man!");
         break;
     }
 
-    if (guess < 0 || guess > 6 ){
-        alert("Error! Please Enter a Valid Number.")
-    
+
+    if (guess < 0 || guess > 6) {
+        alert ("Please enter a valid number!")
     } else {
-        guesses += 1;
+        guesses = guesses + 1;
 
-        if (guess == location1 || guess == location2  || guess == location3){
-            hits +=1;
-            alert("Hit!");
-        
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+            alert("Hit!")
+            
 
-
-            if (hits == 3){
+            if (hits == 3) {
                 isSunk = true;
-                alert (" You Sunk the Ship!")
-
+                alert("You sank my battleship!")
             }
-        }else{
-                alert("Miss!")
-            }
-            }
-                
-
+        }else {
+            alert("Miss!")
         }
-var accuracy = (guesses > 0 ? (hits/guesses) *100: 0).toFixed(2);
-    
+    }
+}
 
+var accuracy = (guesses > 0 ? (hits / guesses) * 100 : 0).toFixed(2);
 
-var stats = "You sank my battership at " + guesses + " guesses" + "\n"+
-"accuracy: " + accuracy + "%"
+var stats = "You took " + guesses + " guesses to sink the battleship!" + "\n" + "Accuracy: " + accuracy + "%";
 
 alert(stats);
